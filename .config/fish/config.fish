bfa17f0c30abf0d33e586257d8fedb51c84b5175
= <m.kjellberg@gmail.com>
2025-08-14 19:32:56 +0200

    More updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fastfetch
+    atuin init fish | source
 end
 
 abbr -a spm sudo pacman -S
+
+zoxide init fish | source
+atuin init fish | source
